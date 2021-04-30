25595f9e3f58ef398742c062eb843cdcbf98dcad
miryambathilde <mbathilde@gmail.com>
2021-04-30 13:47:29 +0200

    definimos varibales + metodo TirarDados + click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda = "../assets/img/dice1.png";
+  dadoDerecha = "../assets/img/dice4.png";
+  numero1!: number;
+  numero2!: number;
+
+
+  /* metodo para random en boton de iniciar tirada entre numeros 1 y 6, ponemos *5 + 1 para evitar que salga el 0*/
+  /* void es porque no va a devolver ni recibir nada */
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    console.log(this.numero1!);
+    console.log(this.numero2!);
+  }
 }
