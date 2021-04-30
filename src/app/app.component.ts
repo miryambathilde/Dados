5e6ea01dcd176c09c3cc686ad051ab9680adc093
miryambathilde <mbathilde@gmail.com>
2021-04-30 14:07:09 +0200

    numero1 numero2 en ts inicia x defecto con 1 y 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   dadoIzquierda = "../assets/img/dice1.png";
   dadoDerecha = "../assets/img/dice4.png";
-  numero1!: number;
-  numero2!: number;
+  /* indicamos que se inicie el numero1 con el 1 y el numero2 con el 2 para que no se muestre el has ganado por defecto */
+  numero1= 1;
+  numero2= 2;
 
 
   /* metodo para random en boton de iniciar tirada entre numeros 1 y 6, ponemos *5 + 1 para evitar que salga el 0*/
@@ -17,7 +18,8 @@ export class AppComponent {
   tirarDados(): void {
     this.numero1 = Math.round(Math.random() * 5) + 1;
     this.numero2 = Math.round(Math.random() * 5) + 1;
-    console.log(this.numero1!);
-    console.log(this.numero2!);
+    /* le decimos cada dado que corresponde a la ruta relativa sin definir una imagen fija, más el random del numero 1 y 2 + .png */
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png'
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png'
   }
 }
